6c02cbf9b293f13bb096d9eb3aab1a81164b70b6
ahmadreza <ahmadreza@webina.ir>
2021-04-10 20:15:13 +0430

    SuperAdmin can change his profile

--- resources/views/admin/admins/index.blade.php
+++ resources/views/admin/admins/index.blade.php
@@ -46,6 +46,115 @@
                             </tr>
                             </thead>
                             <tbody >
+                            @foreach (\App\Models\User::where('id',Auth::user()->id)->get() as $user)
+                                <tr>
+                                    <td>
+                                        <img class="rounded-circle" src="{{URL::to('/').$user->profile()}}" alt="" width="50" height="50">
+                                    </td>
+
+                                    <td>
+                                        {{$user->name}}
+                                    </td>
+                                    <td>{{$user->email}}</td>
+                                    <td>
+                                        @foreach ($user->role as $r)
+                                            {{$r->name}}
+                                        @endforeach
+                                    </td>
+                                    <td >
+                                        <button class="btn btn-default" data-toggle="tooltip" data-placement="top" title="{{$user->created_at}}">
+                                            {{\Morilog\Jalali\Jalalian::forge($user->created_at)->format('%A, %d %B %y')}}
+                                        </button>
+                                    </td>
+                                    <td class="text-center">
+                                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            <i class="fas fa-sliders-h"></i>
+                                        </button>
+                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                            <button type="button" class="btn btn-success dropdown-item" data-toggle="modal" data-target="#modal-edit{{$user->id}}" ><i  class="fas fa-user-edit"></i> ویرایش</button>
+                                        </div>
+                                    </td>
+
+                                </tr>
+                                <!-- /.modal -->
+                                <!-- /.modal -->
+                                <!-- Change Modal -->
+                                <div class="modal fade" id="modal-edit{{$user->id}}">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h4 class="modal-title">ویرایش مدیر</h4>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <!-- form start -->
+                                                <form  method="POST" action="{{route('admin.update',$user->id)}}"  enctype="multipart/form-data">
+                                                    @csrf
+                                                    @method('PATCH')
+                                                    <input type="hidden" name="id" value="{{$user->id}}">
+                                                    <div class="card-body">
+                                                        <div class="form-group">
+                                                            <label for="exampleInputEmail1">نام</label>
+                                                            <input name="name" type="text" class="form-control" id="exampleInputEmail1" placeholder="Enter name" required value="{{$user->name}}" required>
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label for="exampleInputEmail1">ایمیل</label>
+                                                            <input name="email" type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email" required value="{{$user->email}}" required>
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label for="exampleInputPassword1">رمز عبور جدید</label>
+                                                            <input name="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label for="exampleInputPassword1">تکرار رمز عبور</label>
+                                                            <input type="password" name="re_password" class="form-control" id="exampleInputPassword1" placeholder="Password" >
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label for="exampleFormControlSelect1">انتخاب نقش</label>
+                                                            <select class="form-control" name="role" id="exampleFormControlSelect1">
+                                                                <option value="0"  selected disabled>انتخاب نقش</option>
+                                                                @foreach($roles as $role)
+                                                                    <option value="{{$role->name}}"
+                                                                            @if($user->role)
+                                                                            {{-- {{dd($user->role->pluck('name')->first())}} --}}
+                                                                            @if($role->name == $user->role->pluck('name')->first())
+                                                                            selected
+                                                                        @endif
+                                                                        @endif
+                                                                    >{{$role->name}}</option>
+                                                                @endforeach
+                                                            </select>
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label for="exampleInputFile">تصویر پروفایل</label>
+                                                            <div class="input-group">
+                                                                <div class="custom-file">
+                                                                    <input type="file" name="img" class="custom-file-input" id="exampleInputFile">
+                                                                    <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <!-- /.card-body -->
+
+                                                    <div class="card-footer">
+                                                        <button type="submit" class="btn btn-primary">ویرایش</button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                        <!-- /.modal-content -->
+                                    </div>
+                                    <!-- /.modal-dialog -->
+                                </div>
+                                <!-- /.modal -->
+
+
+
+                            @endforeach
+
                             @foreach ($admins as $user)
                                 <tr>
                                     <td>
