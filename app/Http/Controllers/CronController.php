8b8b3de7a50f98f2d6b0494bbde9a5701eaa1865
ahmadreza <ahmadreza@webina.ir>
2021-05-13 17:20:05 +0430

    only admin can fetch content

--- app/Http/Controllers/CronController.php
+++ app/Http/Controllers/CronController.php
@@ -4,8 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Doc;
+use Illuminate\Support\Facades\Auth;
+
 class CronController extends Controller
 {
+    public function __construct()
+    {
+
+        $this->middleware(function ($request, $next) {
+            if (Auth::user()->isSuperAdmin())
+            {
+                return $next($request);
+            }else{
+                abort(404);
+            }
+        });
+
+    }
     public function fetchContent(Request $request)
     {
         Doc::query()->each(function (Doc $doc) {
