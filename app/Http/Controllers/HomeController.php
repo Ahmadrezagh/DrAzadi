b4d2d49b9618a640ab55cf97d4e3456012851049
ahmadreza <ahmadreza@webina.ir>
2021-05-28 21:49:57 +0430

    brands order by position

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -41,7 +41,6 @@ class HomeController extends Controller
      */
     public function index(Request $request)
     {
-        Content::find(137)->getBrands();
         $key = CacheKey($request,Auth::user()->default->type ?? 0);
         if(!Cache::has($key))
         {
